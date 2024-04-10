3eb41214d437ab6eaab51644461824ea1ac89e99
Ryan Epis <louisedepone@gmail.com>
2024-04-10 10:54:34 +0800

    adding anchor tag link

--- src/components/Navigation/Top_Navigation.jsx
+++ src/components/Navigation/Top_Navigation.jsx
@@ -132,7 +132,7 @@ export default function TopNavigation() {
           onClick={() => {
             localStorage.removeItem("token");
           }}
-          href="https://irateteacher.onrender.com/"
+          href="https://irateteacher.onrender.com/login"
         >
           <LogOut color="white" />
         </a>
